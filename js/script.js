24e2ffd4970c130042fdc945194eda53df2494ed
Magnus R. Souza <magnus.rs@hotmail.com>
2023-07-25 22:37:08 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*Arquivo com codificação dos componentes */
+function topo(){
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:'smooth'
+        }
+    )
+}
